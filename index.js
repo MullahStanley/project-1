8338c841cbcd6b993973b05fc090b52eabba92d5
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-04-05 22:37:49 +0300

    Working

--- index.js
+++ index.js
@@ -1,44 +1,27 @@
-window.addEventListener("DOMContentLoaded",appendCarousel(meals))
+const base_src="http://localhost:3000/meals";
 function appendCarousel(meals){
     let div = document.createElement("div")
     div.innerHTML=`
-    <div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="${meals.id}" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="${meals.id}" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="${meals.id}" aria-label="Slide 3"></button>
+    <div class="w3-card-4">
+  <div class="w3-container w3-center">
+    <h2>${meals.name}</h2>
+    <h3>${meals.country}</h3>
+    <p>${meals.description}</p>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">vie
-      <img src="${meals.image}" class="d-block w-100" alt="${meals.name}">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>${meals.country}</h5>
-        <h4>${meals.name}</h4>
-        <p>${meals.description}.</p>
-      </div>
-    </div>
+  <img src="${meals.image}" alt="${meals.name}">
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
     `
-    document.querySelector("#id-1").appendChild(div)
+    document.querySelector("#div-1").appendChild(div)
 }
 
 //fetching
-function fetchMeals(meals){
-    fetch("http://localhost:3000/meals")
+function fetchMeals(){
+    fetch(base_src)
     .then(res => res.json())
-    .then(data =>()=>{
-        data.array.forEach(meals => {
-            appendCarousel(meals)
-        });
+    .then((meals)=>{
+      meals.forEach((meal)=>{
+        appendCarousel(meal)
+      })
     })
-
 }
+fetchMeals()
