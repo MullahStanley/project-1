d42cddf354babcc308bed0b04f7ca3d924fc6766
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-04-08 14:38:02 +0300

    Debug

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function appendCarousel(meals) {
   div.innerHTML = `
     <div class="carousel-inner" style="padding:10px;">
     <div class="carousel-item active">
-      <img src="${meals.image}" class="d-block w-100" alt="${meals.name}">
-      <div class="carousel-caption d-none d-md-block">
+      <img src="${meals.image}" class="d-block w-100" alt="${meals.name}" title="${meals.description}">
+      <div class="carousel-caption d-none d-md-block" id="divi">
         <h>${meals.country}.</h3>
         <h5>${meals.name}.</h5>
         <p>${meals.description}.</p>
@@ -14,16 +14,6 @@ function appendCarousel(meals) {
     </div>
     `
   document.querySelector("#div-1").appendChild(div)
-}
-//prev and next
-function nextSlide() {
-  fetch("http://localhost:3000/meals/id")
-    .then(res => res.json())
-    .then((meals) => {
-      meals.forEach((id) => {
-        prevSlide(id)
-      })
-    })
 }
 //fetching
 function fetchMeals() {
@@ -37,80 +27,3 @@ function fetchMeals() {
 }
 fetchMeals()
 
-//table for displaying data
-function tableAdd(meals) {
-  let tableRow = document.createElement("tr")
-  tableRow.id = "table-row"
-  tableRow.innerHTML = `
-  <th scope="row">${meals.id}</th>
-  <td>${meals.country}</td>
-  <td>${meals.name}</td>
-  <td>${meals.image}</td>
-  <td>${meals.description}</td>
-  <td><button class="btn" style="background-colour:green;" id="edit">Edit</button></td>
-  <td><button class="btn" style="background-colour:red;" id="delete">Remove</button></td>
-  `
-  document.querySelector("#table1").appendChild(tableRow)
-  tableRow.querySelector("#edit").addEventListener("click", () => {
-    updateMeals(meals.id)
-  })
-  tableRow.querySelector("delete").addEventListener("click", () => {
-    tableRow.remove()
-    deleteRecord(meals.id)
-  })
-}
-
-//collect form data
-let formData;
-function gatherInfo() {
-  const form = document.querySelector("#form")
-  form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    formData = {
-      name: e.target.name.value,
-      country: e.target.country.value,
-      image: e.target.image.value,
-      description: e.target.description.value
-    }
-    postMeals()
-  })
-}
-gatherInfo()
-//POST
-function postMeals() {
-  fetch(base_src, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(formData)
-  })
-    .then(response => response.json())
-    .then(data => console.log(data))
-}
-postMeals()
-//updating meals
-function updateMeals() {
-  fetch(base_src, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify()
-  })
-    .then(res => res.json())
-    .then(meals => console.log(meals))
-}
-updateMeals()
-//deleting meals
-function deleteRecord() {
-  fetch(`${base_src / id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(res => res.json())
-    .then(meals => console.log(meals))
-}
-deleteRecord()
---
+++ table.js
@@ -0,0 +1,90 @@
+//table for displaying data
+function tableAdd(meals) {
+    let tableRow = document.createElement("tr")
+    tableRow.id = "table-row"
+    tableRow.innerHTML = `
+    <th scope="row">${meals.id}</th>
+    <td>${meals.country}</td>
+    <td>${meals.name}</td>
+    <td>${meals.image}</td>
+    <td>${meals.description}</td>
+    <td><button class="btn" style="background-colour:green;" id="edit">Edit</button></td>
+    <td><button class="btn" style="background-colour:red;" id="delete">Remove</button></td>
+    `
+    document.querySelector("#tble").appendChild(tableRow)
+    tableRow.querySelector("#edit").addEventListener("click", (e) => {
+      e.preventDefault()
+      updateMeals()
+    })
+    tableRow.querySelector("#delete").addEventListener("click", () => {
+      tableRow.remove()
+      deleteRecord()
+    })
+  }
+  //fetching
+function fetchTable() {
+  fetch("http://localhost:3000/meals")
+    .then(res => res.json())
+    .then((meals) => {
+      meals.forEach((meal) => {
+        tableAdd(meal)
+      })
+    })
+}
+fetchTable()
+  //collect form data
+  let formData;
+  function gatherInfo() {
+    const form = document.querySelector("#btn1")
+    form.addEventListener("submit", (e) => {
+      e.preventDefault()
+      formData = {
+        Country: e.target.country,
+        Food: e.target.name,
+        Image: e.target.image,
+        Description: e.target.description
+      }
+      postMeals()
+    })
+  }
+  gatherInfo()
+  //POST
+  function postMeals() {
+    fetch("http://localhost:3000/meals", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(meals => (meals))
+  }
+  postMeals()
+  //updating meals
+  function updateMeals() {
+    fetch("http://localhost:3000/meals", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({})
+    })
+      .then(res => res.json())
+      .then(data => (data))
+  }
+  updateMeals()
+  //deleting meals
+  function deleteRecord() {
+    fetch("http://localhost:3000/meals", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify()
+    })
+      .then(res => res.json())
+      .then(data => (data))
+  }
+  deleteRecord()
+  
